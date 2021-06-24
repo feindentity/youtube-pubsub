8c4e7d961bc8a18cc559449a5b988aa314201926
mike feindentity <mike@feindentity.net>
2021-06-23 21:52:44 -0400

    first commit

---
+++ publishers/user-publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+namespace pub
+{
+    class Program
+    {
+        static readonly HttpClient client=new HttpClient();
+        static readonly string daprPort =Environment.GetEnvironmentVariable("DAPR_HTTP_PORT");
+        static void Main(string[] args)
+        {
+            for(int counter=0; counter <10;counter++)
+            {
+             var data =$"{{\"action\":\"add\",\"id\":\"{counter}\"}}";
+             System.Console.WriteLine($"Sending {data}");
+             var content = new StringContent(data, System.Text.Encoding.UTF8,"application/json");
+             var response = client.PostAsync($"http://localhost:{daprPort}/v1.0/publish/pubsub/above-store-user",content).Result;
+            }   
+        }
+            
+    }
+}
---
+++ subscribers/sub/Controllers/ProcessController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+
+        private readonly ILogger<ProcessController> _logger;
+
+        public ProcessController(ILogger<ProcessController> logger)
+        {
+            _logger = logger;
+        }
+
+      [HttpPost("/Process")]
+      public void Run([FromBody] object body)
+      {
+          _logger.LogInformation(body.ToString());
+      }
+    }
+}
