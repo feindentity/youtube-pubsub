d2d0de554bae07f9285daf815f75b4a4d0e54af6
feindentity <81498334+feindentity@users.noreply.github.com>
2021-07-02 02:25:41 +0000

    With Service Bus libraries registered for sample

---
+++ publishers/sb-publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sb_publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ subscribers/user-subscriber-2/Controllers/ProcessController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace user_subscriber_2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+
+        private readonly ILogger<ProcessController> _logger;
+
+        public ProcessController(ILogger<ProcessController> logger)
+        {
+            _logger = logger;
+        }
+
+      [HttpPost("/Process")]
+      public void Run([FromBody] object body)
+      {
+          _logger.LogInformation("Subscriber-2:" + body.ToString());
+      }
+    }
+}
