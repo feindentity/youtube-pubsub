657b86923bbc62af4a8353afa11eb06028139850
feindentity <81498334+feindentity@users.noreply.github.com>
2021-07-07 12:15:41 +0000

    Update to add sb-publisher

--- publishers/sb-publisher/Program.cs
+++ publishers/sb-publisher/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 namespace sb_publisher
 {
-    class Program
+
+//write to a topic 
+    public class sbpub
     {
-        static void Main(string[] args)
+        public async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString = "Endpoint=sb://pubsubpoc.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Yy4+uTSv/6TN0gy5FvDRATUTOufM0FPECOT3CNPl1I8=";
+            var topicName = "sb-publisher-Code-spaces";
+            var sbClient = new ServiceBusClient(connectionString);
+            var sbSender = sbClient.CreateSender(topicName);
+            var numOfMessages = 3;
+           
+                       using ServiceBusMessageBatch messageBatch = await sbSender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus topic
+                await sbSender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the topic.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sbSender.DisposeAsync();
+                await sbClient.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
         }
     }
 }
